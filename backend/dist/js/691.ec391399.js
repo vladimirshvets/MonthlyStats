564ded61521312b1868634af284e9b0fe8005927
Vladimir Shvets <uladzimir.shvets@gmail.com>
2023-03-30 23:31:17 +0300

    Frontend build

--- backend/dist/js/691.ec391399.js
+++ backend/dist/js/691.ec391399.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[691],{691:function(n,e,t){t.r(e),t.d(e,{default:function(){return d}});var u=t(3396);function a(n,e,t,a,c,r){return(0,u.wg)(),(0,u.iD)("h1",null,"404 Page Not Found")}var c={name:"NotFound"},r=t(89);const o=(0,r.Z)(c,[["render",a],["__scopeId","data-v-7d896c5a"]]);var d=o}}]);
-//# sourceMappingURL=691.cfc5b907.js.map
+//# sourceMappingURL=691.ec391399.js.map
